b6142924c0095beb98b37bf441e2d9030a4b17d9
przemyk94 <p.jedryszczak@gmail.com>
2019-05-26 21:54:53 +0200

    Initial commit

---
+++ poketimes/src/components/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rainbow from '../hoc/Rainbow'
+const About =() =>{
+    return(
+        <div className="container">
+            <h4 className="center"> About</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum maxime distinctio ipsum inventore, molestias placeat sint quam nulla enim atque facilis incidunt velit earum. Ipsa explicabo commodi ipsam corrupti eum?</p>
+        </div>
+    )
+}
+export default Rainbow(About)
---
+++ poketimes/src/components/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react';
+const Contact =() =>{
+    
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum maxime distinctio ipsum inventore, molestias placeat sint quam nulla enim atque facilis incidunt velit earum. Ipsa explicabo commodi ipsam corrupti eum?</p>
+        </div>
+    )
+}
+export default Contact
